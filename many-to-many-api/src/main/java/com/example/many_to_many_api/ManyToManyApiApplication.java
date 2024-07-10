09d1ee3d6dfe64b4192e6acfe25e5be067510564
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-10 10:16:57 +0000

    add backend file

---
+++ many-to-many-api/src/main/java/com/example/many_to_many_api/ManyToManyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.many_to_many_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyToManyApiApplication.class, args);
+	}
+
+}
